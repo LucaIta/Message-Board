e12ab5800f536735f4152ea172c106eca5f84c72
Luca Quatela <lucaqq@gmail.com>
2016-06-09 21:16:03 -0700

    add routes and models

---
+++ app/models/question.js
@@ -0,0 +1,13 @@
+import Model from 'ember-data/model';
+// import attr from 'ember-data/attr';
+// import { belongsTo, hasMany } from 'ember-data/relationships';
+
+export default Model.extend({
+
+});
+
+
+// question itself
+// author
+// additional notes
+// answers
