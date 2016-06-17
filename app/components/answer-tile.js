474b9fd0e32f335ab4ed338986638ed4a9982da6
Luca Quatela <lucaqq@gmail.com>
2016-06-17 09:14:46 -0700

    add computed property which display answers based on the number of upvotes

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  sortBy: ['votes:desc'],
+  sortedAnswer: Ember.computed.sort('answers', 'sortBy'),
+
   actions: {
     upvote(answer){
       this.sendAction('upvote', answer)
